8154dfac8f53936bdfebbe71a7b64fe9448f799a
serfoll <62280198+serfoll@users.noreply.github.com>
2020-05-29 10:26:33 +0100

    Quitt, Restart and End

--- The Mayan Mousetrap/Assets/Scripts/Game Manager/GameManager.cs
+++ The Mayan Mousetrap/Assets/Scripts/Game Manager/GameManager.cs
@@ -1,13 +1,22 @@
-﻿using System.Xml.Serialization;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public enum GameStatus { Over, Complete }
+
+    private GameStatus gameStatus;
+
     public LevelComplete levelComplete;
     public TimerControl timerCtrl;
     public TrapsController trapsCtrl;
     public CharacterCondition characterCondition;
     public CharacterConditionController characterConditionCtrl;
+
+    //GameComponents
+    public GameObject gOUI, gCUI;
+
+    bool restart;
 
     void Start()
     {
@@ -20,6 +29,17 @@ public class GameManager : MonoBehaviour
         if (levelComplete.finished)
         {
             timerCtrl.timerStop = true;
+            SetGameStatus(GameStatus.Complete);
+        }
+
+        if (characterConditionCtrl.currentHealth <= 0)
+        {
+            SetGameStatus(GameStatus.Over);
+        }
+
+        if (restart)
+        {
+            Time.timeScale = 1;
         }
 
     }
@@ -29,5 +49,37 @@ public class GameManager : MonoBehaviour
         characterConditionCtrl.currentHealth -= d;
         characterCondition.healthSlider.value = characterConditionCtrl.currentHealth;
         Debug.Log(characterConditionCtrl.currentHealth);
+    }
+
+    public void SetGameStatus( GameStatus status)
+    {
+        switch (status)
+        {
+            case GameStatus.Over:
+                {
+                    Time.timeScale = 0;
+                    gOUI.SetActive(true);
+                    break;
+                }
+            case GameStatus.Complete:
+                {
+                    Time.timeScale = 0;
+                    gCUI.SetActive(true);
+                    break;
+                }
+        }
+    }
+
+    public void RestartGame()
+    {
+        restart = true;
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+        Debug.Log(scene.name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
